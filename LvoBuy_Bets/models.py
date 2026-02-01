6fa83c454c7caeae8387be4f78b3cd09eca6fa32
bulgarmapper@gmail.com <bulgarmapper@gmail.com>
2026-02-01 23:29:02 +0200

    Add Listing class

--- LvoBuy_Bets/models.py
+++ LvoBuy_Bets/models.py
@@ -1,6 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 # Create your models here.
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField()
+    image_url = models.URLField(blank=True)
+    current_bid = models.FloatField()
+
+    seller = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="listings"
+    )
+
+    highest_bidder = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="winning_bids"
+    )
