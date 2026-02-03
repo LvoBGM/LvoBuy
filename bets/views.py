dfeae2d3c5cb9c4ebe7a65a9c18b0cf443c6c08e
bulgarmapper@gmail.com <bulgarmapper@gmail.com>
2026-02-03 09:12:06 +0200

    feat: add views and urls needed for user auth

--- bets/views.py
+++ bets/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "bets/index.html")
+
+def login_view(request):
+    return render(request, "bets/login.html")
+
+def logout_view(request):
+    ...
+
+def register_view(request):
+    return render(request, "register.html"),
