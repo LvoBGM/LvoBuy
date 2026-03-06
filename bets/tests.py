8a9ecd9085cd32e0a94f84fdfa292b38f5ebc185
bulgarmapper@gmail.com <bulgarmapper@gmail.com>
2026-03-06 21:16:18 +0200

    test: add test for accessing new-listing page

--- bets/tests.py
+++ bets/tests.py
@@ -8,6 +8,7 @@ class AccessTests(TestCase):
     def setUp(self):
         self.login_url = reverse('login')
         self.register_url = reverse('register')
+        self.new_listing_url = reverse('new_listing')
         self.home_url = reverse('home_specific_page', args=[1])
 
         self.user = User.objects.create_user(
@@ -23,6 +24,21 @@ class AccessTests(TestCase):
     def test_anonymous_user_can_access_register(self):
         """Anonymous users can access register page."""
         response = self.client.get(self.register_url)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_anonymous_user_redirected_from_new_listing(self):
+        """Anonymous users will be redirected when accessings new listing page."""
+        response = self.client.get(self.new_listing_url)
+
+        expected_url = f"{self.login_url}?next={self.new_listing_url}"
+        self.assertRedirects(response, expected_url)
+    
+    def test_logged_in_user_can_access_new_listing(self):
+        """Logged in users can access new listing page."""
+        self.client.login(username='testuser', password='krastavica')
+
+        response = self.client.get(self.new_listing_url)
+
         self.assertEqual(response.status_code, 200)
 
     def test_logged_in_user_redirected_from_login(self):
